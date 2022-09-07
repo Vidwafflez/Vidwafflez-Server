45037d9db5fa60b5299ccdf2c50017a636a863e6
Coffee <99076687+YukisCoffee@users.noreply.github.com>
2022-09-07 20:18:16 +0900

    [api] Finish up base API validation behaviours

--- modules/Vidwafflez/Api/Base/Api.php
+++ modules/Vidwafflez/Api/Base/Api.php
@@ -2,6 +2,8 @@
 // Copyright 2022 Vidwafflez GK.
 namespace Vidwafflez\Api\Base;
 
+use Exception;
+
 /**
  * Implements base API behaviours.
  */
@@ -36,14 +38,79 @@ class Api
         return (object)$response;
     }
 
+    /**
+     * Validate an API request.
+     */
     public static function validateRequest(&$req, $schema)
     {
+        // Convert types accordingly
         self::convertRequestType($req);
+        if (!is_array($schema)) throw new Exception(
+            "API schema must be of type array."
+        );
+
+        // Iterate the request properties and validate that
+        // non-optional ones are there
+        foreach ($schema as $key => $props)
+        {
+            $exists = isset($req->{$key});
+            $optional = $props["optional"] ?? false;
+
+            // Validate types (including "any" special type)
+            if (isset($props["type"]))
+            {
+                $type = $props["type"];
+
+                if ("any" != $type && self::getType($req->{$key}) != $type)
+                {
+                    self::invalidate("Invalid type for property $key.");
+                    return false;
+                }
+            }
+
+            if (!$exists && !$optional)
+            {
+                self::invalidate("Request is missing required property $key.");
+                return false;
+            }
+        }
+
+        // If we got to this point, we've succeeded, so respond
+        // true.
+        return true;
     }
 
+    /**
+     * Get the last invalidation reason (for reporting API errors)
+     */
     public static function getLastInvalidationReason()
     {
         return self::$lastInvalidationReason;
+    }
+
+    /**
+     * Set the invalidation reason.
+     */
+    protected static function invalidate($reason)
+    {
+        self::$lastInvalidationReason = $reason;
+    }
+
+    /**
+     * Get the type name for type handlers
+     */
+    protected static function getType($v)
+    {
+        $nativeType = gettype($v);
+
+        switch ($nativeType)
+        {
+            case "boolean": return "bool";
+            case "integer": return "int";
+            case "double":  return "float";
+            case "NULL":    return "null";
+            default:        return $nativeType;
+        }
     }
 
     /**
