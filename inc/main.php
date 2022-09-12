c235e127ad2e80a526c250e6619b4f6962972940
Coffee <99076687+YukisCoffee@users.noreply.github.com>
2022-09-12 21:29:15 +0900

    [php] Setup composer, twig

--- inc/main.php
+++ inc/main.php
@@ -6,4 +6,5 @@
  * This controls the most basic behaviours used by all Vidwafflez
  * components, such as the autoloader and project-wide constants.
  */
+include "modules/composer/autoload.php" or die("Run composer before running server.");
 require "autoloader.php";
